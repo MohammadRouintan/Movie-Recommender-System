dfe1b3b305bfdba04245450b1d0d850adcd94cf2
Mohammad Rouintan <mdrn1381@gmail.com>
2023-07-06 22:03:51 +0330

    final edit

--- App/app.py
+++ App/app.py
@@ -2,114 +2,84 @@ import streamlit as st
 import requests
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
-from PIL import Image
-from io import BytesIO
+from model import Model
 import concurrent.futures
 
-movies_meta = pd.read_csv('data/MetadataBasedRecommenderData.csv')
-movies_meta['combine'] = movies_meta['combine'].fillna('')
-tfidf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0.0, stop_words='english')
-tfidf_matrix = tfidf.fit_transform(movies_meta['combine'])
+
 movies_list = pd.read_csv('data/movies_list.csv')
 movies_list = movies_list.reset_index()
-titles = movies_list['title']
-ids = movies_list['id']
 
-def calculate_tfidf(data):
-    tfidf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0.0, stop_words='english')
-    tfidf_matrix = tfidf.fit_transform(data)
-    return tfidf_matrix
+st.markdown('<div style="text-align: center;"><h1>Movie Recommender System</h1></div>', unsafe_allow_html=True)
 
-def api_poster(moive_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=6b8ac0d80d854198821514621e36ae32&language=en-US".format(moive_id)
-    headers = {"accept": "application/json"}
-    response = requests.get(url, headers=headers)
-    response = response.json()
-
-    full_path = 'data/No-Image-Placeholder.png'
-    if 'poster_path' in response:
-        poster_path = response['poster_path']
-        if not (poster_path == np.nan or poster_path == None): 
-            full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-
-    return full_path
-
-
-def combine_recommendation(title, movie_indices):
-    idx = movies_list[movies_list['title'] == title].index[0]
-    cosine_sim = cosine_similarity(tfidf_matrix[int(idx)], tfidf_matrix)
-    similarity_scores = list(enumerate(cosine_sim[0]))
-    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
-    similarity_scores = similarity_scores[1:501]
-    movie_indices_combine = [i[0] if i[0] in movie_indices else None for i in similarity_scores]
-
-    names = set()
-    for i in movie_indices_combine:
-        if i != None:
-            names.add(titles.iloc[i])
-    return names, set(movie_indices_combine)
-
-
-def recommender(title):
-    idx = movies_list[movies_list['title'] == title].index[0]
-    cosine_sim = cosine_similarity(tfidf_matrix[int(idx)], tfidf_matrix)
-    similarity_scores = list(enumerate(cosine_sim[0]))
-    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
-    similarity_scores = similarity_scores[1:501]
-    movie_indices = [i[0] for i in similarity_scores]
-
-    posters = []
-    for i in movie_indices:
-        posters.append(api_poster(ids.iloc[i]))
-    names = []
-    for i in movie_indices:
-        names.append(titles.iloc[i])
-    return names, posters, movie_indices
-
-# tab1, tab2, tab3 = st.tabs(['Model1', 'Model2', 'Model3'])
-
-# with tab1:
-st.header('Movie Recommender System')
+selected_user = st.text_input("Type User Id Between 1-671", key=0)
 selected_movie1 = st.selectbox(
-    "Type or select first movie from the dropdown",
+    "Type or select first movie",
     movies_list['title'], key=1
 )
 
 selected_movie2 = st.selectbox(
-    "Type or select second movie from the dropdown",
+    "Type or select second movie",
     movies_list['title'], key=2
 )
 
 selected_movie3 = st.selectbox(
-    "Type or select third movie from the dropdown",
+    "Type or select third movie",
     movies_list['title'], key=3
 )
 
-if st.button('Recommend', key=10):
-    pool = concurrent.futures.ThreadPoolExecutor(max_workers=10)
-    
-    recommended_movie_names1, recommended_movie_posters1, movies_indices1 = pool.submit(recommender ,selected_movie1)
-    recommended_movie_names2, recommended_movie_posters2, movies_indices2 = pool.submit(recommender ,selected_movie2)
-    recommended_movie_names3, recommended_movie_posters3, movies_indices3 = pool.submit(recommender ,selected_movie3)
-    
-    pool.shutdown(wait=True)
-    
+
+c1, c2, c3 = st.columns(3)
+number = 0
+with c1:
+    if st.button('Recommend By Movie', key=10):
+        number = 1
+
+with c2:
+    if st.button('Recommend By User', key=11):
+        number = 2
+
+with c3:
+    if st.button('Recommend By User & Movie', key=12):
+        number = 3
+
+def correct_userId(string):
+    try:
+        user_id = int(string)
+        if 1 <= user_id <= 671:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+if number == 1:
+    model = Model('Movie')
+    pool1 = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+    future1 = pool1.submit(model.recommender, selected_movie1)
+    future2 = pool1.submit(model.recommender, selected_movie2)
+    future3 = pool1.submit(model.recommender, selected_movie3)
+    recommended_movie_names1, recommended_movie_posters1, movies_indices1 = future1.result()
+    recommended_movie_names2, recommended_movie_posters2, movies_indices2 = future2.result()
+    recommended_movie_names3, recommended_movie_posters3, movies_indices3 = future3.result()
+    pool1.shutdown(wait=True)
+
     pool2 = concurrent.futures.ThreadPoolExecutor(max_workers=10)
-    set1, indices1 = pool2.submit(combine_recommendation ,selected_movie1, movies_indices2 + movies_indices3)
-    set2, indices2 = pool2.submit(combine_recommendation ,selected_movie2, movies_indices1 + movies_indices3)
-    set3, indices3 = pool2.submit(combine_recommendation ,selected_movie3, movies_indices1 + movies_indices2)\
-        
+    future1 = pool2.submit(model.combine_recommendation, selected_movie1, movies_indices2 + movies_indices3)
+    future2 = pool2.submit(model.combine_recommendation, selected_movie2, movies_indices1 + movies_indices3)
+    future3 = pool2.submit(model.combine_recommendation, selected_movie3, movies_indices1 + movies_indices2)
+    set1, indices1 = future1.result()
+    set2, indices2 = future2.result()
+    set3, indices3 = future3.result()
     pool2.shutdown(wait=True)
 
     intersection_names = list(set1 & set2 & set3)
     indices = list(indices1 & indices2 & indices3)
     intresection_posters = []
     if intersection_names:
+        st.markdown('<div style="text-align: center;"><h4>Combination Of Three Movie Recommended</h4></div>', unsafe_allow_html=True)
         for idx in indices:
             if idx != None:
-                intresection_posters.append(api_poster(ids.iloc[idx]))
+                intresection_posters.append(model.api_poster(model.model_data['id'].iloc[idx]))
 
         cols = st.columns(5)
         for i in range(len(intersection_names)):
@@ -120,8 +90,7 @@ if st.button('Recommend', key=10):
                     st.text(intersection_names[i])
                     st.image(intresection_posters[i])
                 
-
-
+    st.markdown('<div style="text-align: center;"><h5>Recommended to First Movie</h5></div>', unsafe_allow_html=True)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommended_movie_names1[0])
@@ -139,6 +108,7 @@ if st.button('Recommend', key=10):
         st.text(recommended_movie_names1[4])
         st.image(recommended_movie_posters1[4])
 
+    st.markdown('<div style="text-align: center;"><h5>Recommended to Second Movie</h5></div>', unsafe_allow_html=True)
     col5, col6, col7, col8, col9 = st.columns(5)
     with col5:
         st.text(recommended_movie_names2[0])
@@ -156,6 +126,7 @@ if st.button('Recommend', key=10):
         st.text(recommended_movie_names2[4])
         st.image(recommended_movie_posters2[4])
 
+    st.markdown('<div style="text-align: center;"><h5>Recommended to Third Movie</h5></div>', unsafe_allow_html=True)
     col10, col11, col12, col13, col14 = st.columns(5)
     with col10:
         st.text(recommended_movie_names3[0])
@@ -172,152 +143,55 @@ if st.button('Recommend', key=10):
     with col14:
         st.text(recommended_movie_names3[4])
         st.image(recommended_movie_posters3[4])
+elif number == 2:
+    if not correct_userId(selected_user):
+        st.markdown('<div style="text-align: center;"><h4>Please Enter Correct User Id</h4></div>', unsafe_allow_html=True)
+    else:
+        model = Model('User')
+        movies = model.recommender(userId=int(selected_user))
+        movies_names = movies['title'].values.tolist()
+        movies_id = movies['id'].values.tolist()
+        posters = []
+        if movies_names:
+            for idx in movies_id:
+                if idx != None:
+                    posters.append(model.api_poster(idx))
 
-# with tab2:
-#     st.header('Movie Recommender System')
-#     selected_movie1 = st.selectbox(
-#         "Type or select first movie from the dropdown",
-#         movies_list['title'], key=4
-#     )
+            cols = st.columns(5)
+            for i in range(len(movies_names)):
+                if i < 10:
+                    if i % 5 == 0:
+                        cols = st.columns(5)
+                    with cols[i - (i // 5)*5]:
+                        st.text(movies_names[i])
+                        st.image(posters[i])
+elif number == 3:
+    if not correct_userId(selected_user):
+        st.markdown('<div style="text-align: center;"><h4>Please Enter Correct User Id</h4></div>', unsafe_allow_html=True)
+    else:
+        model = Model('User & Movie')
+        pool1 = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+        future1 = pool1.submit(model.recommender, selected_movie1, int(selected_user))
+        future2 = pool1.submit(model.recommender, selected_movie2, int(selected_user))
+        future3 = pool1.submit(model.recommender, selected_movie3, int(selected_user))
+        recommended_movie1 = future1.result()
+        recommended_movie2 = future2.result()
+        recommended_movie3 = future3.result()
+        pool1.shutdown(wait=True)
+        recommended_movie = pd.concat([recommended_movie1, recommended_movie2, recommended_movie3], ignore_index=True)
+        movies_names = recommended_movie['title'].values.tolist()
+        movies_id = recommended_movie['id'].values.tolist()
+        posters = []
+        if movies_names:
+            for idx in movies_id:
+                if idx != None:
+                    posters.append(model.api_poster(idx))
 
-#     selected_movie2 = st.selectbox(
-#         "Type or select second movie from the dropdown",
-#         movies_list['title'], key=5
-#     )
-
-#     selected_movie3 = st.selectbox(
-#         "Type or select third movie from the dropdown",
-#         movies_list['title'], key=6
-#     )
-
-#     if st.button('Recommend', key=11):
-#         recommended_movie_names1, recommended_movie_posters1 = recommender(selected_movie1)
-#         recommended_movie_names2, recommended_movie_posters2 = recommender(selected_movie2)
-#         recommended_movie_names3, recommended_movie_posters3 = recommender(selected_movie3)
-
-#         col1, col2, col3, col4, col5 = st.columns(5)
-#         with col1:
-#             st.text(recommended_movie_names1[0])
-#             st.image(recommended_movie_posters1[0])
-#         with col2:
-#             st.text(recommended_movie_names1[1])
-#             st.image(recommended_movie_posters1[1])
-#         with col3:
-#             st.text(recommended_movie_names1[2])
-#             st.image(recommended_movie_posters1[2])
-#         with col4:
-#             st.text(recommended_movie_names1[3])
-#             st.image(recommended_movie_posters1[3])
-#         with col5:
-#             st.text(recommended_movie_names1[4])
-#             st.image(recommended_movie_posters1[4])
-
-#         col5, col6, col7, col8, col9 = st.columns(5)
-#         with col5:
-#             st.text(recommended_movie_names2[0])
-#             st.image(recommended_movie_posters2[0])
-#         with col6:
-#             st.text(recommended_movie_names2[1])
-#             st.image(recommended_movie_posters2[1])
-#         with col7:
-#             st.text(recommended_movie_names2[2])
-#             st.image(recommended_movie_posters2[2])
-#         with col8:
-#             st.text(recommended_movie_names2[3])
-#             st.image(recommended_movie_posters2[3])
-#         with col9:
-#             st.text(recommended_movie_names[4])
-#             st.image(recommended_movie_posters[4])
-
-#         col10, col11, col12, col13, col14 = st.columns(5)
-#         with col10:
-#             st.text(recommended_movie_names3[0])
-#             st.image(recommended_movie_posters3[0])
-#         with col11:
-#             st.text(recommended_movie_names3[1])
-#             st.image(recommended_movie_posters3[1])
-#         with col12:
-#             st.text(recommended_movie_names3[2])
-#             st.image(recommended_movie_posters3[2])
-#         with col13:
-#             st.text(recommended_movie_names3[3])
-#             st.image(recommended_movie_posters3[3])
-#         with col14:
-#             st.text(recommended_movie_names3[4])
-#             st.image(recommended_movie_posters3[4])
-
-# with tab3:
-#     st.header('Movie Recommender System')
-
-#     selected_movie1 = st.selectbox(
-#         "Type or select first movie from the dropdown",
-#         movies_list['title'], key=7
-#     )
-
-#     selected_movie2 = st.selectbox(
-#         "Type or select second movie from the dropdown",
-#         movies_list['title'], key=8
-#     )
-
-#     selected_movie3 = st.selectbox(
-#         "Type or select third movie from the dropdown",
-#         movies_list['title'], key=9
-#     )
-
-#     if st.button('Recommend', key=12):
-#         recommended_movie_names1, recommended_movie_posters1 = recommender(selected_movie1)
-#         recommended_movie_names2, recommended_movie_posters2 = recommender(selected_movie2)
-#         recommended_movie_names3, recommended_movie_posters3 = recommender(selected_movie3)
-
-#         col1, col2, col3, col4, col5 = st.columns(5)
-#         with col1:
-#             st.text(recommended_movie_names1[0])
-#             st.image(recommended_movie_posters1[0])
-#         with col2:
-#             st.text(recommended_movie_names1[1])
-#             st.image(recommended_movie_posters1[1])
-#         with col3:
-#             st.text(recommended_movie_names1[2])
-#             st.image(recommended_movie_posters1[2])
-#         with col4:
-#             st.text(recommended_movie_names1[3])
-#             st.image(recommended_movie_posters1[3])
-#         with col5:
-#             st.text(recommended_movie_names1[4])
-#             st.image(recommended_movie_posters1[4])
-
-#         col5, col6, col7, col8, col9 = st.columns(5)
-#         with col5:
-#             st.text(recommended_movie_names2[0])
-#             st.image(recommended_movie_posters2[0])
-#         with col6:
-#             st.text(recommended_movie_names2[1])
-#             st.image(recommended_movie_posters2[1])
-#         with col7:
-#             st.text(recommended_movie_names2[2])
-#             st.image(recommended_movie_posters2[2])
-#         with col8:
-#             st.text(recommended_movie_names2[3])
-#             st.image(recommended_movie_posters2[3])
-#         with col9:
-#             st.text(recommended_movie_names2[4])
-#             st.image(recommended_movie_posters2[4])
-
-#         col10, col11, col12, col13, col14 = st.columns(5)
-#         with col10:
-#             st.text(recommended_movie_names3[0])
-#             st.image(recommended_movie_posters3[0])
-#         with col11:
-#             st.text(recommended_movie_names3[1])
-#             st.image(recommended_movie_posters3[1])
-#         with col12:
-#             st.text(recommended_movie_names3[2])
-#             st.image(recommended_movie_posters3[2])
-#         with col13:
-#             st.text(recommended_movie_names3[3])
-#             st.image(recommended_movie_posters3[3])
-#         with col14:
-#             st.text(recommended_movie_names3[4])
-#             st.image(recommended_movie_posters3[4])
-
-    
+            cols = st.columns(5)
+            for i in range(len(movies_names)):
+                if i < 10:
+                    if i % 5 == 0:
+                        cols = st.columns(5)
+                    with cols[i - (i // 5)*5]:
+                        st.text(movies_names[i])
+                        st.image(posters[i])
--- App/model.py
+++ App/model.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
-from nltk.stem.snowball import SnowballStemmer
 from sklearn.preprocessing import MinMaxScaler
 from surprise import Reader, Dataset, SVD
 from surprise.model_selection import cross_validate
+import requests
+import joblib
+import concurrent.futures
 
 class Model:
 
@@ -17,18 +19,44 @@ class Model:
             self.model_data = pd.read_csv('data/MovieBasedRecommender.csv')
         elif model_name == 'User':
             self.model_name = model_name
-            self.model_data = pd.read_csv('data/UserBasedRecommender.csv')
+            self.model_data = pd.read_csv('data/MovieBasedRecommender.csv')
+            self.model_svd = joblib.load('model/SVD.joblib')
+            self.model_id = pd.read_csv('data/movie_id.csv')
+            self.model_ratings = pd.read_csv('data/ratings_small.csv')
+        elif model_name == 'User & Movie':
+            self.model_name = model_name
+            self.model_data = pd.read_csv('data/MovieBasedRecommender.csv')
+            self.model_svd = joblib.load('model/SVD.joblib')
+            self.model_id = pd.read_csv('data/movie_id.csv')
+            self.model_ratings = pd.read_csv('data/ratings_small.csv')
 
+        self.model_feature = self.model_data['model_feature'].fillna('')
+        self.tfidf_matrix = self.calculate_tfidf(self.model_feature)
 
-    def calculate_tfidf(data):
+    def api_poster(self, moive_id):
+        url = "https://api.themoviedb.org/3/movie/{}?api_key=6b8ac0d80d854198821514621e36ae32&language=en-US".format(moive_id)
+        headers = {"accept": "application/json"}
+        response = requests.get(url, headers=headers)
+        response = response.json()
+
+        full_path = 'data/No-Image-Placeholder.png'
+        if 'poster_path' in response:
+            poster_path = response['poster_path']
+            if not (poster_path == np.nan or poster_path == None): 
+                full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+
+        return full_path
+
+    def calculate_tfidf(self, data):
         tfidf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0.0, stop_words='english')
         tfidf_matrix = tfidf.fit_transform(data)
         return tfidf_matrix
 
 
-    def combine_recommendation(title, movie_indices):
-        idx = movies_list[movies_list['title'] == title].index[0]
-        cosine_sim = cosine_similarity(tfidf_matrix[int(idx)], tfidf_matrix)
+    def combine_recommendation(self, title, movie_indices):
+        data = self.model_data.reset_index()
+        idx = data[data['title'] == title].index[0]
+        cosine_sim = cosine_similarity(self.tfidf_matrix[int(idx)], self.tfidf_matrix)
         similarity_scores = list(enumerate(cosine_sim[0]))
         similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
         similarity_scores = similarity_scores[1:501]
@@ -37,21 +65,76 @@ class Model:
         names = set()
         for i in movie_indices_combine:
             if i != None:
-                names.add(titles.iloc[i])
+                names.add(self.model_data['title'].iloc[i])
         return names, set(movie_indices_combine)
 
-    def recommender(title):
-        idx = movies_list[movies_list['title'] == title].index[0]
-        cosine_sim = cosine_similarity(tfidf_matrix[int(idx)], tfidf_matrix)
-        similarity_scores = list(enumerate(cosine_sim[0]))
-        similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
-        similarity_scores = similarity_scores[1:501]
-        movie_indices = [i[0] for i in similarity_scores]
+    def hybrid_svd(self, userId, title):
+        data = self.model_data.reset_index()
+        idx = data[data['title'] == title].index[0]
+        cosine_sim = cosine_similarity(self.tfidf_matrix[int(idx)], self.tfidf_matrix)
+        sim_scores = list(enumerate(cosine_sim[0]))
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = sim_scores[1:20]
+        movie_indices = [i[0] for i in sim_scores]
 
-        posters = []
-        for i in movie_indices:
-            posters.append(api_poster(ids.iloc[i]))
-        names = []
-        for i in movie_indices:
-            names.append(titles.iloc[i])
-        return names, posters, movie_indices
+        movies = self.model_data.iloc[movie_indices][['title', 'id']]
+        tempLinks = self.model_id[self.model_id['tmdbId'].isin(movies['id'].tolist())]  
+        l = list() 
+        
+        for item in  tempLinks["movieId"]:
+                l.append (self.model_svd.predict(userId, item).est)
+        
+        tempData = pd.DataFrame({"title" :movies['title'],"est": l, "id" :movies['id']})
+        tempData = tempData.sort_values('est', ascending=False)
+        tempData = tempData[tempData["est"] >= 3]
+
+        return tempData
+    
+    def recommender(self, title=None, userId=None, similarity_weight=0.9, top_n=1000):
+        if self.model_name == 'Movie':
+            data = self.model_data.reset_index()
+            idx = data[data['title'] == title].index[0]
+            cosine_sim = cosine_similarity(self.tfidf_matrix[int(idx)], self.tfidf_matrix)
+            similarity = cosine_sim[0].T
+
+            sim_data = pd.DataFrame(similarity, columns=['similarity'])
+            final_data = pd.concat([data, sim_data], axis=1)
+
+            final_data['final_score'] = final_data['score']*(1-similarity_weight) + final_data['similarity']*similarity_weight
+
+            final_data_sorted = final_data.sort_values(by='final_score', ascending=False).head(top_n)
+            self_index = final_data_sorted[final_data_sorted['title'] == title].index
+            final_data_sorted.drop(self_index, inplace=True)
+            remove_indices = final_data_sorted[final_data_sorted['similarity'] < 0.01].index
+            final_data_sorted.drop(remove_indices, inplace=True)
+            movies_indices = final_data_sorted.index.tolist()
+
+            posters = []
+            for i in movies_indices[:10]:
+                posters.append(self.api_poster(self.model_data['id'].iloc[i]))
+
+            names = []
+            for i in movies_indices[:10]:
+                names.append(self.model_data['title'].iloc[i])
+            
+            return names, posters, movies_indices
+        elif self.model_name == 'User':
+            tempRatings = self.model_ratings[self.model_ratings['userId'] == userId]
+            tempRatings = tempRatings[tempRatings["rating"] >= 3.5]
+            
+            tempLinks = self.model_id[self.model_id['tmdbId'].isin(tempRatings['movieId'].tolist())]  
+            titlesData = self.model_data[self.model_data["id"].isin(tempLinks["tmdbId"])]
+            
+            resultDataFrame = pd.DataFrame()
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                futures = []
+                for i in titlesData["title"]:
+                    futures.append(executor.submit(self.hybrid_svd, userId, i))
+                for future in concurrent.futures.as_completed(futures):
+                    try:
+                        resultDataFrame = pd.concat([future.result(), resultDataFrame], ignore_index = True)
+                    except:
+                        print('connect time out')
+            return resultDataFrame.sort_values('est', ascending=False)
+        elif self.model_name == 'User & Movie':
+            return self.hybrid_svd(userId=userId, title=title)
